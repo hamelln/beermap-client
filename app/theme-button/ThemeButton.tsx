aed6807a6b2e254d5bc1c7cd53faa0354a7f392b
hamelln <vusdus91@gmail.com>
2023-08-07 14:40:33 +0900

    fix: 예전에 쓰던 코드로 복구 - state 초기값을 확정하지 않는 코드여서 두 번 클릭이 강제됨

--- app/theme-button/ThemeButton.tsx
+++ app/theme-button/ThemeButton.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import S from "./ThemeButton.module.scss";
 import SunIcon from "../icons/SunIcon";
 import MoonIcon from "../icons/MoonIcon";
 
 const ThemeButton = () => {
-  const [theme, setTheme] = useState<string>("");
+  const savedTheme = localStorage.getItem("theme") ?? "light";
+  const [theme, setTheme] = useState<string>(savedTheme);
 
   const changeTheme = (theme: string) => {
     document.body.setAttribute("data-theme", theme);
@@ -16,18 +17,15 @@ const ThemeButton = () => {
     if (theme === "light") {
       setTheme("dark");
       localStorage.setItem("theme", "dark");
-      changeTheme("dark");
     } else {
       setTheme("light");
       localStorage.removeItem("theme");
-      changeTheme("light");
     }
   };
 
-  useLayoutEffect(() => {
-    const savedTheme = localStorage.getItem("theme") ?? "light";
-    changeTheme(savedTheme);
-  }, []);
+  useEffect(() => {
+    changeTheme(theme);
+  }, [theme]);
 
   return (
     <div className={S.main} onClick={handleTheme}>
