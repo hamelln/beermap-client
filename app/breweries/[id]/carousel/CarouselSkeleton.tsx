a73e6b004db092bc8aeef9cc2fce68ee6352130c
hamelln <vusdus91@gmail.com>
2023-08-08 13:54:10 +0900

    fix: 캐러셀 스켈레톤을 상호작용만 빼고 캐러셀과 스타일과 똑같이 적용

--- app/breweries/[id]/carousel/CarouselSkeleton.tsx
+++ app/breweries/[id]/carousel/CarouselSkeleton.tsx
@@ -4,12 +4,20 @@ import BackArrowIcon from "@/app/icons/BackArrowIcon";
 
 const CarouselSkeleton = () => {
   return (
-    <section className={S.main} style={{ height: "calc(100vw * 8/9)" }}>
-      <button className={S.prev_page_arrow}>
+    <section className={S.main}>
+      <button className={S.prev_page_arrow} aria-label="뒤로 가기">
         <BackArrowIcon />
       </button>
-      <div className={S.spinner}></div>
-      <div className={S.indicator}></div>
+      <div>
+        <img
+          className={S.carousel_image}
+          src="/brewery-image.webp"
+          alt="brewery image"
+          fetchPriority="high"
+        />
+        <div className={S.carosel_background}></div>
+      </div>
+      <div className={S.indicator}>1 / 1</div>
     </section>
   );
 };
