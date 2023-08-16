eb5704a8a728a18c803ab42cc1b043cde4972937
hamelln <vusdus91@gmail.com>
2023-08-16 16:30:56 +0900

    feat: 닫기 함수를 Props로 받아서 닫기 버튼에 주입, 정보창 html 추가

--- app/[id]/naver-map/NaverMaps.tsx
+++ app/[id]/naver-map/NaverMaps.tsx
@@ -9,6 +9,9 @@ import {
 import S from "./NaverMaps.module.scss";
 import useDebounce from "@/utils/useDebounce";
 import MouseClick from "@/types/MouseClick";
+import CopyIcon from "@/app/icons/CopyIcon";
+import NavigationIcon from "@/app/icons/NavigationIcon";
+import CloseIcon from "@/app/icons/CloseIcon";
 
 interface Props {
   isMapOpen: boolean;
@@ -16,6 +19,7 @@ interface Props {
   fullAddress: string;
   latitude: number;
   longitude: number;
+  handleMap: () => void;
 }
 
 const NaverMaps = ({
@@ -24,13 +28,14 @@ const NaverMaps = ({
   fullAddress,
   latitude,
   longitude,
+  handleMap,
 }: Props) => {
   if (!isMapOpen) return <></>;
   const [showNotification, setShowNotification] = useState(false);
   const debouncedSetShowNotification = useDebounce(() => {
     setShowNotification(false);
   });
-  const ADDRESS_BOX_HEIGHT_RATIO = 0.2;
+  const ADDRESS_BOX_HEIGHT_RATIO = 0.18;
   const LATITUDE_SPAN = 0.005;
   const LATITUDE_ADJUSTMENT = LATITUDE_SPAN * ADDRESS_BOX_HEIGHT_RATIO;
 
@@ -62,16 +67,18 @@ const NaverMaps = ({
     <NavermapsProvider
       ncpClientId={process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID!}
     >
-      <div className={S.map_box}>
+      <section className={S.map_box}>
         <Container className={S.map}>
           <BreweryPlace />
         </Container>
-        <div className={S.place_description_box}>
+        <aside className={S.place_description_box}>
           <h3 className={S.place_name}>{breweryName}</h3>
           <div className={S.place_address_box} onClick={handleClick}>
-            <p className={S.place_address}>{fullAddress}</p>
-            {/* <img className={S.place_copy_svg}></img> */}
-            <p className={S.place_copy_text}>복사</p>
+            <p className={S.place_address}>
+              {fullAddress}
+              <CopyIcon />
+              <span className={S.place_copy_text}>복사</span>
+            </p>
           </div>
           <div
             className={`${S.notification} ${
@@ -80,9 +87,16 @@ const NaverMaps = ({
           >
             주소를 복사했습니다.
           </div>
-          <div onClick={getDirections}>길찾기</div>
-        </div>
-      </div>
+          <nav className={S.nav_box}>
+            <div className={S.nav_inner_box} onClick={getDirections}>
+              <NavigationIcon />
+            </div>
+            <div className={S.nav_inner_box} onClick={handleMap}>
+              <CloseIcon />
+            </div>
+          </nav>
+        </aside>
+      </section>
     </NavermapsProvider>
   );
 };
