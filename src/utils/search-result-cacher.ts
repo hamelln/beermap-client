606da11dcf80c41cab71d5470ad4206b284259a7
hamelln <vusdus91@gmail.com>
2023-08-18 17:28:50 +0900

    fix: 데이터 업데이트 주기 1/2로 축소, 세션 스토리지 값 삭제 안 하기로 변경 - 어차피 세션 닫으면 사라질 값이므로, 없애는 게 더 기능 혼란만 줄 듯함

--- src/utils/search-result-cacher.ts
+++ src/utils/search-result-cacher.ts
@@ -15,19 +15,16 @@ export const saveBreweries = (breweries: IBrewery[]) => {
 
 export const loadScrollPosition = (): number => {
   const scrollY: SessionValue = sessionStorage.getItem("scrollPosition");
-  sessionStorage.removeItem("scrollPosition");
   return scrollY ? Number(scrollY) : 0;
 };
 
 export const loadKeyword = (): string => {
   const keyword: SessionValue = sessionStorage.getItem("keyword");
-  sessionStorage.removeItem("keyword");
   return keyword ?? "";
 };
 
 export const loadBreweries = (): IBrewery[] => {
   const encodedBreweries: SessionValue = sessionStorage.getItem("brewery-list");
-  sessionStorage.removeItem("brewery-list");
   if (encodedBreweries) {
     const decodedBreweries = JSON.parse(encodedBreweries);
     return decodedBreweries;
