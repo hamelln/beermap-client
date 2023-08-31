44f82c666978c3fe932d38bed593c9dfffd4066e
hamelln <vusdus91@gmail.com>
2023-08-31 12:55:15 +0900

    refactor: isMapOpen 제거, useModal로 자체 관리하도록 변경 - 기존 방식은 모달창이 열리기 전에도 미리 렌더링해두는 결함 있음 - 모달 컴포넌트: 모달이 열릴 때에만 내부 컴포넌트가 렌더링

---
+++ src/components/brewery-details/contact/Contact.tsx
@@ -0,0 +1,202 @@
+"use client";
+
+import React, { ReactElement } from "react";
+import S from "./Contact.module.scss";
+import LocationIcon from "src/components/icons/LocationIcon";
+import PhoneIcon from "src/components/icons/PhoneIcon";
+import LinkIcon from "src/components/icons/LinkIcon";
+import InstagramIcon from "src/components/icons/InstagramIcon";
+import BreweryDetailsProps from "@/types/BreweryDetailsProps";
+import ClockIcon from "src/components/icons/ClockIcon";
+import ChevronIcon from "src/components/icons/ChevronIcon";
+import NaverMaps from "../naver-map/NaverMaps";
+import { NavermapsProvider } from "react-naver-maps";
+import Modal from "@/components/modal/Modal";
+import ModalLink from "@/components/modal/ModalLink";
+import useModal from "@/utils/useModal";
+
+interface Props
+  extends Pick<
+    BreweryDetailsProps,
+    | "breweryName"
+    | "phone"
+    | "websiteType"
+    | "websiteUrl"
+    | "officeHours"
+    | "summarizedOfficeHours"
+    | "latitude"
+    | "longitude"
+  > {
+  fullAddress: string;
+}
+
+const Contact = ({
+  breweryName,
+  fullAddress,
+  phone,
+  websiteType,
+  websiteUrl,
+  officeHours,
+  summarizedOfficeHours,
+  latitude,
+  longitude,
+}: Props) => {
+  const phoneNumber = phone.replaceAll("-", "");
+  const today = new Date().getDay();
+  const day = ["일", "월", "화", "수", "목", "금", "토"][today];
+  const operatingHours = officeHours[day];
+  const { openTime, closeTime, breakTime, lastOrder } = operatingHours;
+  const officeHourModalProps = useModal("office_hour");
+  const mapModalProps = useModal("map");
+
+  const summarizeHourBoxStyle = {
+    padding: "0.6rem",
+    lineHeight: "20px",
+  };
+
+  const summarizeCloseButtonStyle = {
+    border: "2px solid var(--color-font-primary)",
+    borderRadius: "var(--border-radius-primary)",
+    padding: "10px",
+  };
+
+  const createHourBox = (
+    days: string,
+    officeHour: string,
+    breakTime: string
+  ) => {
+    if (officeHour === "closed") {
+      return (
+        <div style={summarizeHourBoxStyle} key={days}>
+          <p>매주 {days}요일은 쉽니다</p>
+        </div>
+      );
+    }
+    if (breakTime === "none") {
+      return (
+        <div style={summarizeHourBoxStyle} key={days}>
+          <p>{days}</p>
+          <time>{officeHour}</time>
+        </div>
+      );
+    }
+    return (
+      <div style={summarizeHourBoxStyle} key={days}>
+        <p>{days}</p>
+        <time>{officeHour}</time>
+        <p>브레이크 타임</p>
+        <time>{breakTime}</time>
+      </div>
+    );
+  };
+
+  const temp: {
+    openDayComponents: ReactElement[];
+    closedComponent: ReactElement;
+  } = {
+    openDayComponents: [],
+    closedComponent: <></>,
+  };
+
+  const officeHourComponents = summarizedOfficeHours.reduce(
+    (acc, summarizedOfficeHour) => {
+      const [days, officeHour, breakTime] = summarizedOfficeHour;
+      if (officeHour === "closed") {
+        acc.closedComponent = createHourBox(days, officeHour, breakTime);
+      } else {
+        acc.openDayComponents.push(createHourBox(days, officeHour, breakTime));
+      }
+      return acc;
+    },
+    temp
+  );
+
+  const WebsiteIcon =
+    websiteType === "홈페이지" ? <LinkIcon /> : <InstagramIcon />;
+  const BreakTime = breakTime ? (
+    <div className={S.content}>
+      <div>브레이크 타임</div>
+      <time>
+        {breakTime.startTime} - {breakTime.endTime}
+      </time>
+    </div>
+  ) : (
+    <></>
+  );
+  const LastOrder = lastOrder ? (
+    <div className={S.content}>
+      <div>라스트 오더</div>
+      <time>{lastOrder}</time>
+    </div>
+  ) : (
+    <></>
+  );
+
+  return (
+    <section className={S.main}>
+      <ModalLink modalKey={mapModalProps.modalKey}>
+        <address className={S.address_box}>
+          <LocationIcon />
+          <span>{fullAddress}</span>
+        </address>
+      </ModalLink>
+      <NavermapsProvider
+        ncpClientId={process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID!}
+      >
+        <Modal modalProps={mapModalProps}>
+          <NaverMaps
+            breweryName={breweryName}
+            fullAddress={fullAddress}
+            latitude={latitude}
+            longitude={longitude}
+            handleModal={mapModalProps.closeModal}
+          />
+        </Modal>
+      </NavermapsProvider>
+      <div className={S.office_hours_box}>
+        <ClockIcon />
+        <details>
+          <summary>
+            <p>오늘({day})</p>
+            <div className={S.office_hours_inner_box}>
+              {openTime !== "closed" ? (
+                <time>
+                  {openTime} - {closeTime}
+                </time>
+              ) : (
+                <span className={S.closed}>정기 휴무일</span>
+              )}
+              <div>
+                <ChevronIcon />
+              </div>
+            </div>
+          </summary>
+          {BreakTime}
+          {LastOrder}
+          <div className={S.content}>
+            <ModalLink modalKey={officeHourModalProps.modalKey}>
+              다른 날 영업 시간 확인
+            </ModalLink>
+            <Modal modalProps={officeHourModalProps}>
+              <h3 className={S.summarize_title}>영업 시간 안내</h3>
+              {officeHourComponents.openDayComponents}
+              {officeHourComponents.closedComponent}
+            </Modal>
+          </div>
+        </details>
+      </div>
+      <div className={S.phone_box}>
+        <PhoneIcon />
+        <a href={`tel:${phoneNumber}`}>{phone}</a>
+      </div>
+      <div className={S.site_box}>
+        {WebsiteIcon}
+        <a href={websiteUrl} target="_blank">
+          {websiteType}
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/brewery-details/naver-map/NaverMaps.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import React, { MouseEvent, useState } from "react";
+import { Container, NaverMap, Marker, useNavermaps } from "react-naver-maps";
+import S from "./NaverMaps.module.scss";
+import useDebounce from "@/utils/useDebounce";
+import CopyIcon from "src/components/icons/CopyIcon";
+import NavigationIcon from "src/components/icons/NavigationIcon";
+import CloseIcon from "src/components/icons/CloseIcon";
+import MyLocationIcon from "src/components/icons/MyLocationIcon";
+
+interface Props {
+  breweryName: string;
+  fullAddress: string;
+  latitude: number;
+  longitude: number;
+  handleModal: () => void;
+}
+
+const NaverMaps = ({
+  breweryName,
+  fullAddress,
+  latitude,
+  longitude,
+  handleModal,
+}: Props) => {
+  const NAVIGATION_URL = `https://map.naver.com/index.nhn?elng=${longitude}&elat=${latitude}&pathType=0&showMap=true&etext=${breweryName}&menu=route`;
+  const navermaps = useNavermaps();
+  const [showNotification, setShowNotification] = useState(false);
+  const debouncedSetShowNotification = useDebounce(() => {
+    setShowNotification(false);
+  });
+  const [center, setCenter] = useState([latitude, longitude]);
+  const [markers, setMarkers] = useState([[latitude, longitude]]);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(fullAddress);
+    setShowNotification(true);
+    debouncedSetShowNotification();
+  };
+
+  const findClient = (e: MouseEvent) => {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const [lat, lng] = [pos.coords.latitude, pos.coords.longitude];
+      const newMarkers = [...markers, [lat, lng]];
+      setCenter([lat, lng]);
+      setMarkers(newMarkers);
+    });
+  };
+
+  return (
+    <section className={S.map_box}>
+      <Container className={S.map} id="map">
+        <NaverMap
+          center={new navermaps.LatLng(center[0], center[1])}
+          zoomControl
+          zoomControlOptions={{
+            position: navermaps.Position.RIGHT_CENTER,
+            style: navermaps.ZoomControlStyle.SMALL,
+          }}
+          zoom={17}
+          minZoom={7}
+          maxZoom={21}
+          disableKineticPan={true}
+          tileTransition={true}
+        >
+          {markers.map((marker, index) => {
+            console.log(123);
+
+            return (
+              <Marker
+                key={index}
+                position={new navermaps.LatLng(marker[0], marker[1])}
+              />
+            );
+          })}
+        </NaverMap>
+      </Container>
+      <aside className={S.place_description_box}>
+        <button onClick={findClient}>
+          <MyLocationIcon />
+        </button>
+        <h3>{breweryName}</h3>
+        <div className={S.place_address_box} onClick={handleClick}>
+          <p>
+            {fullAddress}
+            <CopyIcon />
+            <span>복사</span>
+          </p>
+        </div>
+        <div
+          className={`${S.notification} ${
+            showNotification && S.show_notification
+          }`}
+        >
+          주소를 복사했습니다.
+        </div>
+        <nav className={S.nav_box}>
+          <a href={NAVIGATION_URL} target="_blank">
+            <NavigationIcon />
+            길찾기
+          </a>
+          <button onClick={handleModal}>
+            <CloseIcon />
+            닫기
+          </button>
+        </nav>
+      </aside>
+    </section>
+  );
+};
+
+export default NaverMaps;
