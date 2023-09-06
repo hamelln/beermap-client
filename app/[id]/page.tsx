926f17e0ad1ace36eafb7a308f0dc8f67080cafd
hamelln <vusdus91@gmail.com>
2023-09-06 20:31:07 +0900

    fix: 오타 수정

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -36,7 +36,7 @@ export default async function BreweryDetails({ params }: Props) {
   } = brewery;
   const fullAddress = `${stateProvince} ${city} ${address}`;
   const carouselImages: Img[] = images ?? [];
-  const firstImage: string = initialCarouselImage ?? "'./brewery-image.webp";
+  const firstImage: string = initialCarouselImage ?? "/brewery-image.webp";
   const breweryDescriptionTexts = breweryDescription.split("\\n");
   const EnteredBreweryDescription = breweryDescriptionTexts.map(
     (line, index) => {
