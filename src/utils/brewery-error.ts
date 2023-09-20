617330776a1e6b52458069a195a4593b6d9501ab
hamelln <vusdus91@gmail.com>
2023-09-20 22:33:01 +0900

    feat: BreweryError 생성자 추가

--- src/utils/brewery-error.ts
+++ src/utils/brewery-error.ts
@@ -2,4 +2,16 @@ import ErrorBase from "./Error-base";
 
 type ErrorName = "URI_ERROR" | "TYPE_ERROR" | "ERROR";
 
-export class BreweryError extends ErrorBase<ErrorName> {}
+export class BreweryError extends ErrorBase<ErrorName> {
+  constructor({
+    name,
+    message,
+    cause,
+  }: {
+    name: ErrorName;
+    message: string;
+    cause?: any;
+  }) {
+    super({ name, message, cause });
+  }
+}
