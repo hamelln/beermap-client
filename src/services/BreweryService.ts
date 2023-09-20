c9e3777de9935c6a2cb43abc29603cdab5a175fb
hamelln <vusdus91@gmail.com>
2023-09-20 22:33:17 +0900

    feat: 상세 페이지 조회 시 에러 처리 추가

--- src/services/BreweryService.ts
+++ src/services/BreweryService.ts
@@ -1,6 +1,7 @@
 import BreweryServiceConstructor from "@/types/BreweryService";
 import Brewery from "@/types/Brewery";
 import { isBrewery } from "@/utils/type-checker";
+import { BreweryError } from "@/utils/brewery-error";
 
 class BreweryService implements BreweryServiceConstructor {
   private readonly BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -8,10 +9,19 @@ class BreweryService implements BreweryServiceConstructor {
   async fetchBreweriesByInputText(query: string): Promise<Brewery[]> {
     const URI = `${this.BASE_URL}?q=${query}`;
     const options: RequestInit = { method: "POST" };
-    const breweries: Brewery[] = await fetch(URI, options).then((res) =>
-      res.json()
-    );
-    return breweries;
+
+    try {
+      const response = await fetch(URI, options);
+      this.validateResponse(response);
+      const breweryList = response.json();
+      return breweryList;
+    } catch (error) {
+      throw new BreweryError({
+        name: "ERROR",
+        message: "브루어리 조회 중 에러 발생",
+        cause: error,
+      });
+    }
   }
 
   async fetchBreweryById(breweryId: string): Promise<Brewery> {
@@ -19,21 +29,48 @@ class BreweryService implements BreweryServiceConstructor {
     const options: RequestInit = { method: "GET", next: { revalidate: 1200 } };
 
     try {
-      const brewery: unknown = await fetch(URI, options).then((res) =>
-        res.json()
-      );
-      if (isBrewery(brewery)) {
-        return brewery;
-      } else {
-        throw new Error("브루어리 형식이 안 맞습니다.");
-      }
-    } catch (e: any) {
-      if (e.response.status === 404) {
-        throw new Error("찾으시는 브루어리가 없습니다.");
-      } else {
-        throw new Error(e.response.data.message);
-      }
+      const brewery = await this.tryFetchBrewery(URI, options);
+      return brewery;
+    } catch (error) {
+      throw new BreweryError({
+        name: "ERROR",
+        message: "브루어리 조회 중 에러 발생",
+        cause: error,
+      });
     }
+  }
+
+  private async tryFetchBrewery(
+    URI: string,
+    options: RequestInit
+  ): Promise<Brewery> {
+    const response = await fetch(URI, options);
+    this.validateResponse(response);
+    const result: unknown = await response.json();
+    const typedResult = this.validateType<Brewery>(result, isBrewery);
+    return typedResult;
+  }
+
+  private validateResponse(response: Response): void {
+    if (!response.ok) {
+      throw new BreweryError({
+        name: "URI_ERROR",
+        message: "유효한 주소가 아닙니다.",
+      });
+    }
+  }
+
+  private validateType<ResultType>(
+    result: unknown,
+    typeChecker: (result: unknown) => result is ResultType
+  ) {
+    if (typeChecker(result)) {
+      return result;
+    }
+    throw new BreweryError({
+      name: "TYPE_ERROR",
+      message: "데이터 유형이 맞지 않음.",
+    });
   }
 }
 
