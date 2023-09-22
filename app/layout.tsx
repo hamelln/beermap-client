935de9b54828a150e2391ca62f150fb8f6648a92
hamelln <vusdus91@gmail.com>
2023-09-23 00:09:29 +0900

    feat: 메타 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import "./normalize.scss";
 import "./global.scss";
 import ThemeButton from "@/components/ThemeButton";
-
-export const metadata = {
-  title: "BeerMap",
-  description: "맥주 맛집 찾기",
-};
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -14,6 +10,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width" />
+        <meta charSet="utf-8" />
+        <title>비어맵</title>
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="description" content="맛있는 브루어리를 찾아서" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="비어맵" />
+        <meta property="og:description" content="맛있는 브루어리를 찾아서" />
+        <meta property="og:image" content="/image-og-800x400.jpg" />
+        <meta property="og:image:alt" content="BeerMap Image Thumbnail" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:site_name" content="비어맵" />
+        <meta property="og:locale:alternate" content="ko_KR" />
+      </Head>
       <body suppressHydrationWarning={true}>
         <ThemeButton />
         {children}
