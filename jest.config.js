dca381da262a0bf3b25289f4b5ada22aceb4156b
hamelln <vusdus91@gmail.com>
2023-10-02 17:17:58 +0900

    test: 테스트 환경 재설정

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,43 @@
-const nextJest = require('next/jest')
+const esModules = ["slick-carousel"].join("|");
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
-}
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = {
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/coverage/**",
+  ],
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "src/(.*)": "<rootDir>/$1",
+    "^@/app/(.*)": "<rootDir>/app/$1",
+    "^@/components/(.*)": "<rootDir>/src/components/$1",
+    "^@/services/(.*)": "<rootDir>/src/services/$1",
+    "^@/types/(.*)": "<rootDir>/src/types/$1",
+    "^@/utils/(.*)": "<rootDir>/src/utils/$1",
+    "^@/hooks/(.*)": "<rootDir>/src/hooks/$1",
+    "^@/mocks/(.*)": "<rootDir>/__mocks__/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+    "/node_modules/slick-carousel/slick/slick.css",
+    `/node_modules/(?!${esModules})`,
+  ],
+  setupFilesAfterEnv: ["<rootDir>/test-setup.js"],
+};
---
+++ test-setup.js
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
